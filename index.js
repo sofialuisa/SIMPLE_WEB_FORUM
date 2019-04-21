6464352e2989a986be4478c55d05d479fbed6674
Sofía Suazo <sls936@nyu.edu>
2019-04-21 17:21:59 -0400

    trying

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ var app = express();
 var bodyParser = require("body-parser");
 const url = require('url');
 const path = require('path');
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
+var { Client } = require('pg');
+var client = new Client({database: 'simple_web_forum'});
+client.connect();
+
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// var insults_dict = {
-//   0: "bad, stupid and amateur",
-//   1: "awful, predictible and arrogant",
-//   2: "boring, careless and aggresive",
-//   3: "fussy, ugly and noisey",
-//   4: "plain, unoriginal and overrated"
-// };
-
+client.query('SELECT simple_web_forum;', (err, res) => {
+  if (err) throw err;
+  for (let column of res.column) {
+    console.log(JSON.stringify(column));
+  }
+  client.end();
+});
 
 app.get('/', function(req, res){
     var myText = req.query.mytext; //mytext is the name of your input box
